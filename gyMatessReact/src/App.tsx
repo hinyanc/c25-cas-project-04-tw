9e28d242457b311f1edc05ae7dba0f71e8898825
Chinny <learning20150133@gmail.com>
2023-06-06 15:55:27 +0800

    login testing

--- gyMatessReact/src/App.tsx
+++ gyMatessReact/src/App.tsx
@@ -1,118 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
+
+// import React, {useState} from 'react';
+// import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+// export default function App() {
+//   const [todo, setTodos] = useState([
+//     {text: 'buy cofee', key: '1'},
+//     {text: 'buy milk', key: '2'},
+//     {text: 'buy milk cofee', key: '3'},
+//   ]);
+
+//   return (
+//     <View style={styles.container}>
+//       {/* {header} */}
+//       <View style={styles.content}>
+//         {/* {to form} */}
+//         <View style={styles.list}>
+//           <FlatList
+//             data={todo}
+//             renderItem={({item}) => <Text>{item.text}</Text>}
+//           />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+//   content: {
+//     padding: 40,
+//   },list:{
+//     marginTop:20
+//   }
+// });
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Provider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import Login from '../src';
+import {theme} from '../src/core/theme';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Main = () => (
+  <Provider theme={theme}>
+    <NavigationContainer>
+      <Login />
+    </NavigationContainer>
+  </Provider>
+);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step hihi123">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your hihihi">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default Main;
--- gyMatessReact/src/components/Auth.tsx
+++ gyMatessReact/src/components/Auth.tsx
@@ -1,54 +1,54 @@
-import React, { FormEvent, useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../app/store";
-import { login } from "./authSlice";
-import { fetchLogin } from "./authAPI";
+// import React, { FormEvent, useState } from "react";
+// import { Button, Form } from "react-bootstrap";
+// import { useDispatch } from "react-redux";
+// import { AppDispatch } from "../../app/store";
+// import { login } from "./authSlice";
+// import { fetchLogin } from "./authAPI";
 
-export function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPasswrod] = useState("");
+// export function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPasswrod] = useState("");
 
-  const dispatch = useDispatch<AppDispatch>();
+//   const dispatch = useDispatch<AppDispatch>();
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    const result = await fetchLogin(email, password);
-    if (result) {
-      dispatch(login(email));
-    }
-  };
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter username"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </Form.Group>
+//   const handleSubmit = async (e: FormEvent) => {
+//     e.preventDefault();
+//     const result = await fetchLogin(email, password);
+//     if (result) {
+//       dispatch(login(email));
+//     }
+//   };
+//   return (
+//     <Form>
+//       <Form.Group className="mb-3" controlId="formUsername">
+//         <Form.Label>Username</Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="Enter username"
+//           value={email}
+//           onChange={(e) => {
+//             setEmail(e.target.value);
+//           }}
+//         />
+//       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPasswrod(e.target.value);
-          }}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)}>
-        Submit
-      </Button>
-    </Form>
-  );
-}
+//       <Form.Group className="mb-3" controlId="formBasicPassword">
+//         <Form.Label>Password</Form.Label>
+//         <Form.Control
+//           type="password"
+//           placeholder="Password"
+//           value={password}
+//           onChange={(e) => {
+//             setPasswrod(e.target.value);
+//           }}
+//         />
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="formBasicCheckbox">
+//         <Form.Check type="checkbox" label="Check me out" />
+//       </Form.Group>
+//       <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)}>
+//         Submit
+//       </Button>
+//     </Form>
+//   );
+// }
