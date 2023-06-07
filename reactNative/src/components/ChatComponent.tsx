b8fe23c2d27bee957500a7a9d5e5f2fc7a3bccd9
Julia <beyourdetective@gmail.com>
2023-06-07 13:25:16 +0800

    yarn

---
+++ reactNative/src/components/ChatComponent.tsx
@@ -0,0 +1,4 @@
+import { View, Text, Pressable} from "react-native"
+import React, { useLayoutEffect, useState} from "react"; 
+import { useNavigation } from "@react-navigation/native";
+import { styles } from "../utils/styles";
--- reactNative/src/screens/Chat.tsx
+++ reactNative/src/screens/Chat.tsx
@@ -1,62 +1,79 @@
 import React from 'react';
 import {View, Text, Pressable, SafeAreaView, FlatList} from 'react-native';
 import ChatComponent from '../components/ChatComponent';
+import  { Feather }  from 'react-native-feather';
 import {styles} from '../utils/styles';
 
 const Chat = () => {
   //Dummy list of rooms
- const rooms = [
+  const rooms = [
+    {
+      id: '1',
+      name: 'Tecky',
+      messages: [
         {
-            id: "1",
-            name: "Tecky",
-            messages: [
-                {
-                    id: "1a",
-                    text: "Hello, ReactNative is so easy!!",
-                    time: "07:50",
-                    user: "Chinny",
-                },
-                {
-                    id: "1b",
-                    text: "Hi Chinny, I think so! 😇",
-                    time: "08:50",
-                    user: "Yannes",
-                },
-            ],
+          id: '1a',
+          text: 'Hello, ReactNative is so easy!!',
+          time: '07:50',
+          user: 'Chinny',
         },
         {
-            id: "2",
-            name: "GyMatess Admin",
-            messages: [
-                {
-                    id: "2a",
-                    text: "Girls, who's awake? 🙏🏽",
-                    time: "12:50",
-                    user: "Team Julia",
-                },
-                {
-                    id: "2b",
-                    text: "What's up? 🧑🏻‍💻",
-                    time: "03:50",
-                    user: "Chinny",
-                },
-            ],
+          id: '1b',
+          text: 'Hi Chinny, I think so! 😇',
+          time: '08:50',
+          user: 'Yannes',
         },
-    ];
+      ],
+    },
+    {
+      id: '2',
+      name: 'GyMatess Admin',
+      messages: [
+        {
+          id: '2a',
+          text: "Girls, who's awake? 🙏🏽",
+          time: '12:50',
+          user: 'Team Julia',
+        },
+        {
+          id: '2b',
+          text: "What's up? 🧑🏻‍💻",
+          time: '03:50',
+          user: 'Chinny',
+        },
+      ],
+    },
+  ];
 
-    return (
-      <SafeAreaView style={styles.chatscreen}>
-        <View style={styles.chattopContainer}>
-          <View style={styles.chatheader}>
-            <Text style={styles.chatheading}>Chats</Text>
-            {/*  Logs "ButtonPressed" to the console when the icon is clicked */}
+  return (
+    <SafeAreaView style={styles.chatscreen}>
+      <View style={styles.chattopContainer}>
+        <View style={styles.chatheader}>
+          <Text style={styles.chatheading}>Chats</Text>
 
-            <Pressable onPress={() => console.log('ButtonPressed')}>
-              <Icon name="edit" size={24} color="green" />
-            </Pressable>
-          </View>
+          {/* 👇🏻 Logs "ButtonPressed" to the console when the icon is clicked */}
+          <Pressable onPress={() => console.log('Button Pressed!')}>
+            <Feather name="edit" size={24} color="green" />
+          </Pressable>
         </View>
-      </SafeAreaView>
-    );
-  }
+      </View>
+
+      <View style={styles.chatlistContainer}>
+        {rooms.length > 0 ? (
+          <FlatList
+            data={rooms}
+            renderItem={({item}) => <ChatComponent item={item} />}
+            keyExtractor={item => item.id}
+          />
+        ) : (
+          <View style={styles.chatemptyContainer}>
+            <Text style={styles.chatemptyText}>No rooms created!</Text>
+            <Text>Click the icon above to create a Chat room</Text>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
 export default Chat;
