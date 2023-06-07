6c474c477b83213a7d083a3c984a8d2c357eea74
Julia <beyourdetective@gmail.com>
2023-06-07 22:04:36 +0800

    update ts

--- reactNative/src/screens/Chat.tsx
+++ reactNative/src/screens/Chat.tsx
@@ -11,7 +11,7 @@ import socket from '../utils/socket';
 const Chat = () => {
   const [visible, setVisible] = useState(false);
   const [rooms, setRooms] = useState([]);
-  
+
   // Runs when the component mounts
   useLayoutEffect(() => {
     function fetchGroups() {
@@ -29,6 +29,41 @@ const Chat = () => {
       setRooms(rooms);
     });
   }, [socket]);
+
+  const handleCreateGroup = () => setVisible(true);
+
+  return (
+    <SafeAreaView style={styles.chatscreen}>
+      <View style={styles.chattopContainer}>
+        <View style={styles.chatheader}>
+          <Text style={styles.chatheading}>Chats</Text>
+          <Pressable onPress={handleCreateGroup}>
+            <Icon name="edit" size={24} color="green" />
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.chatlistContainer}>
+        {rooms.length > 0 ? (
+          <FlatList
+            data={rooms}
+            renderItem={({item}) => <ChatComponent item={item} />}
+            //  keyExtractor={item => item.id}
+            keyExtractor={(item: {id: any}) => item.id}
+          />
+        ) : (
+          <View style={styles.chatemptyContainer}>
+            <Text style={styles.chatemptyText}>No rooms created!</Text>
+            <Text>Click the icon above to create a Chat room</Text>
+          </View>
+        )}
+      </View>
+      {visible ? <Modal setVisible={setVisible} /> : ''}
+    </SafeAreaView>
+  );
+};
+
+export default Chat;
   //Dummy list of rooms
   // const rooms = [
   //   {
@@ -69,28 +104,28 @@ const Chat = () => {
   //   },
   // ];
 
-      return (
-        <SafeAreaView style={styles.chatscreen}>
-          <View style={styles.chattopContainer}>
-            <View style={styles.chatheader}>
-              <Text style={styles.chatheading}>Chats</Text>
+//       return (
+//         <SafeAreaView style={styles.chatscreen}>
+//           <View style={styles.chattopContainer}>
+//             <View style={styles.chatheader}>
+//               <Text style={styles.chatheading}>Chats</Text>
 
-              {/* Displays the Modal component when clicked */}
-              <Pressable onPress={() => setVisible(true)}>
-                <Icon name="edit" size={24} color="green" />
-              </Pressable>
-            </View>
-          </View>
+//               {/* Displays the Modal component when clicked */}
+//               <Pressable onPress={() => setVisible(true)}>
+//                 <Icon name="edit" size={24} color="green" />
+//               </Pressable>
+//             </View>
+//           </View>
 
-          <View style={styles.chatlistContainer}>...</View>
-          {/*
-                Pass setVisible as prop in order to toggle 
-                the display within the Modal component.
-            */}
-          {visible ? <Modal setVisible={setVisible} /> : ''}
-        </SafeAreaView>
-      );
-};
+//           <View style={styles.chatlistContainer}>...</View>
+//           {/*
+//                 Pass setVisible as prop in order to toggle 
+//                 the display within the Modal component.
+//             */}
+//           {visible ? <Modal setVisible={setVisible} /> : ''}
+//         </SafeAreaView>
+//       );
+// };
 
-export default Chat;
+// export default Chat;
 
--- reactNative/src/screens/Messaging.tsx
+++ reactNative/src/screens/Messaging.tsx
@@ -6,26 +6,23 @@ import {styles} from '../utils/styles';
 import socket from '../utils/socket';
 
 const Messaging = ({route, navigation}: any) => {
+  const [user, setUser] = useState('');
   const {name, id} = route.params;
-
   const [chatMessages, setChatMessages] = useState([
-    {
-      id: '1',
-      text: 'I like React Native!',
-      time: '10:00',
-      user: 'Yannes',
-    },
-    {
-      id: '2',
-      text: 'I think so!',
-      time: '10:10',
-      user: 'Julia',
-    },
+    // {
+    //   id: '1',
+    //   text: 'I like React Native!',
+    //   time: '10:00',
+    //   user: 'Yannes',
+    // },
+    // {
+    //   id: '2',
+    //   text: 'I think so!',
+    //   time: '10:10',
+    //   user: 'Julia',
+    // },
   ]);
   const [message, setMessage] = useState('');
-  const [user, setUser] = useState('');
-
-  // Access the chatroom's name and id from the route object
 
   // This function gets the usersname saved on AsyncStorage
   const getUsername = async () => {
@@ -35,71 +32,44 @@ const Messaging = ({route, navigation}: any) => {
         setUser(valve);
       }
     } catch (error) {
-      console.log(error);
+      console.log('Error while loading username!');
+    }
+  };
+  const handleNewMessage = () => {
+    const hour =
+      new Date().getHours() < 10
+        ? `0${new Date().getHours()}`
+        : `${new Date().getHours()}`;
+
+    const mins =
+      new Date().getMinutes() < 10
+        ? `0${new Date().getMinutes()}`
+        : `${new Date().getMinutes()}`;
+
+    if (user) {
+      socket.emit('newMessage', {
+        message,
+        room_id: id,
+        user,
+        timestamp: {hour, mins},
+      });
     }
   };
 
-  // Set the header title to the name chatroom's name
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      title: name,
-    });
-    getUsername();
-  }, []);
-
-  /* 
-        This function gets the time the user sends a message, then 
-        logs the username, message, and the timestamp to the console.
-     */
-
-const handleNewMessage = () => {
-  const hour =
-    new Date().getHours() < 10
-      ? `0${new Date().getHours()}`
-      : `${new Date().getHours()}`;
-
-  const mins =
-    new Date().getMinutes() < 10
-      ? `0${new Date().getMinutes()}`
-      : `${new Date().getMinutes()}`;
-
-  socket.emit('newMessage', {
-    message,
-    room_id: id,
-    user,
-    timestamp: {hour, mins},
-  });
-
-  
-};
-  // This runs only initial mount
   useLayoutEffect(() => {
     navigation.setOptions({title: name});
-    // Sends the id to the server to fetch all its messages
+    getUsername();
     socket.emit('findRoom', id);
-    socket.on(
-      'foundRoom',
-      (
-        roomChats: React.SetStateAction<
-          {id: string; text: string; time: string; user: string}[]
-        >,
-      ) => setChatMessages(roomChats),
+    socket.on('foundRoom', (roomChats: React.SetStateAction<never[]>) =>
+      setChatMessages(roomChats),
     );
   }, []);
 
-  // This runs when the messages are updated.
   useEffect(() => {
-    socket.on(
-      'foundRoom',
-      (
-        roomChats: React.SetStateAction<
-          {id: string; text: string; time: string; user: string}[]
-        >,
-      ) => setChatMessages(roomChats),
+    socket.on('foundRoom', (roomChats: React.SetStateAction<never[]>) =>
+      setChatMessages(roomChats),
     );
   }, [socket]);
-
-  
 
   return (
     <View style={styles.messagingscreen}>
@@ -114,12 +84,14 @@ const handleNewMessage = () => {
             renderItem={({item}) => (
               <MessageComponent item={item} user={user} />
             )}
-            keyExtractor={item => item.id}
+            // keyExtractor={item => item.id}
+            keyExtractor={(item: {id: any}) => item.id}
           />
         ) : (
           ''
         )}
       </View>
+
       <View style={styles.messaginginputContainer}>
         <TextInput
           style={styles.messaginginput}
@@ -133,9 +105,94 @@ const handleNewMessage = () => {
           </View>
         </Pressable>
       </View>
-      <View style={styles.messagingscreen}>...</View>;
     </View>
   );
 };
 
 export default Messaging;
+//This function gets the time the user sends a message, then logs the username, message, and the timestamp to the console.
+
+// const handleNewMessage = () => {
+//   const hour =
+//     new Date().getHours() < 10
+//       ? `0${new Date().getHours()}`
+//       : `${new Date().getHours()}`;
+
+//   const mins =
+//     new Date().getMinutes() < 10
+//       ? `0${new Date().getMinutes()}`
+//       : `${new Date().getMinutes()}`;
+
+//   socket.emit('newMessage', {
+//     message,
+//     room_id: id,
+//     user,
+//     timestamp: {hour, mins},
+//   });
+
+// };
+//   // This runs only initial mount
+//   useLayoutEffect(() => {
+//     navigation.setOptions({title: name});
+//     // Sends the id to the server to fetch all its messages
+//     socket.emit('findRoom', id);
+//     socket.on(
+//       'foundRoom',
+//       (
+//         roomChats: React.SetStateAction<
+//           {id: string; text: string; time: string; user: string}[]
+//         >,
+//       ) => setChatMessages(roomChats),
+//     );
+//   }, []);
+
+//   // This runs when the messages are updated.
+//   useEffect(() => {
+//     socket.on(
+//       'foundRoom',
+//       (
+//         roomChats: React.SetStateAction<
+//           {id: string; text: string; time: string; user: string}[]
+//         >,
+//       ) => setChatMessages(roomChats),
+//     );
+//   }, [socket]);
+
+//   return (
+//     <View style={styles.messagingscreen}>
+//       <View
+//         style={[
+//           styles.messagingscreen,
+//           {paddingVertical: 15, paddingHorizontal: 10},
+//         ]}>
+//         {chatMessages[0] ? (
+//           <FlatList
+//             data={chatMessages}
+//             renderItem={({item}) => (
+//               <MessageComponent item={item} user={user} />
+//             )}
+//             keyExtractor={item => item.id}
+//           />
+//         ) : (
+//           ''
+//         )}
+//       </View>
+//       <View style={styles.messaginginputContainer}>
+//         <TextInput
+//           style={styles.messaginginput}
+//           onChangeText={value => setMessage(value)}
+//         />
+//         <Pressable
+//           style={styles.messagingbuttonContainer}
+//           onPress={handleNewMessage}>
+//           <View>
+//             <Text style={{color: '#f2f0f1', fontSize: 20}}>SEND</Text>
+//           </View>
+//         </Pressable>
+//       </View>
+//       <View style={styles.messagingscreen}>...</View>;
+//     </View>
+//   );
+// };
+
+// export default Messaging;
