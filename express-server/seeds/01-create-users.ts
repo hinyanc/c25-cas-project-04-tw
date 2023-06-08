924fec0310b82402f5bf6939c14bdae7d79168b0
Chinny <learning20150133@gmail.com>
2023-06-08 18:12:49 +0800

    slove seed ploblem

--- express-server/seeds/01-create-users.ts
+++ express-server/seeds/01-create-users.ts
@@ -107,11 +107,11 @@ export async function seed(knex: Knex): Promise<void> {
   /////////////insert data for goals table///////
 
   for (let i = 1; i < 11; i++) {
-    const height = await this.knex("users")
+    const height = await knex("users")
       .select("height")
       .where("id", "=", `${i}`)
       .first();
-    const weight = await this.knex("users")
+    const weight = await knex("users")
       .select("weight")
       .where("id", "=", `${i}`)
       .first();
