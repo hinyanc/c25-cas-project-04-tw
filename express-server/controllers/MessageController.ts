3c4ec269fa42f2da5a394383ad772f8ba44414ac
Julia <beyourdetective@gmail.com>
2023-06-08 19:12:14 +0800

    app.ts

--- express-server/controllers/MessageController.ts
+++ express-server/controllers/MessageController.ts
@@ -5,5 +5,15 @@ import { logger } from "../utils/logger";
 export class MessageController {
   constructor(private messageService: MessageService) {}
 
-
+  sendController = async (req: Request, res: Response) => {
+    try {
+      const { message } = req.body;
+      const response = await this.messageService.sendMessage(message, +req.user.id);
+      res.status(200).json(response);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  
+  };
 }
--- express-server/routers/messageRoutes.ts
+++ express-server/routers/messageRoutes.ts
@@ -1,2 +1,6 @@
-import { MessageController } from "../controllers/MessageController";
+import { messageController } from "../server";
 import express from "express";
+
+export const messageRoutes = express.Router();
+
+messageRoutes.post("/", messageController.sendController);
