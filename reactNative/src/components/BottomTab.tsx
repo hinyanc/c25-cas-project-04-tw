ab5df0d3fd53a2e6e4c3ed59b056ed215571006d
dora dora <dora_1324@gmail.com>
2023-06-12 21:40:19 +0800

    goal screen

--- reactNative/src/components/BottomTab.tsx
+++ reactNative/src/components/BottomTab.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeDiscoverScreen from '../screens/HomeDiscoverScreen';
 import Chat from '../screens/ChatScreens/Chat';
-import GoalScreen from '../screens/GoalScreen';
+import GoalScreen from '../screens/GoalScreen/GoalScreen';
 import UserScreen from '../screens/ProfileScreens/UserScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {styles} from '../utils/styles';
