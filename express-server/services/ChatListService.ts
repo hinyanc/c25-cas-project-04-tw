a01a7b2a97989ee77a4660b0f57f81a1357f5b28
Julia <beyourdetective@gmail.com>
2023-06-12 20:50:40 +0800

    socket.io

--- express-server/services/ChatListService.ts
+++ express-server/services/ChatListService.ts
@@ -1,28 +1,36 @@
 import type { Knex } from "knex";
 import { chatroomTable } from "../migrations/20230605101740_users";
+import { userTable } from "../migrations/20230605101740_users";
 
 export class ChatListService {
   constructor(private knex: Knex) {}
   getChatHistory = async (mainUserId: number) => {
-    const chatHistory = await this.knex(chatroomTable)
-      .select(
-        "chatroom.id as chat_room_id",
-        "chatroom.updated_at",
-        "users_sender.username as sender_username",
-        "users_receiver.username as receiver_username"
-      )
-      .leftJoin(
-        "users as users_sender",
-        "chatroom.sender_id",
-        "users_sender.id"
-      )
-      .leftJoin(
-        "users as users_receiver",
-        "chatroom.receiver_id",
-        "users_receiver.id"
-      )
-      .andWhere("users_sender.id", mainUserId)
-      .orderBy("chatroom.updated_at", "asc");
+    const chatHistory = await this.knex(userTable)
+      .select("id", "username")
+      .whereIn(
+        "id",
+        this.knex(chatroomTable)
+          .select("sender_id")
+          .orWhere("receiver_id", mainUserId)
+          .distinct()
+      );
+
+    // .distinct("users.id", "users.username")
+    // .select("chatroom.updated_at", "chatroom.message AS last_message")
+    // .innerJoin(chatroomTable, function () {
+    //   this.on("users.id", "=", "chatroom.sender_id").orOn(
+    //     "users.id",
+    //     "=",
+    //     "chatroom.receiver_id"
+    //   );
+    // })
+    // .where(function () {
+    //   this.where("chatroom.sender_id", mainUserId).orWhere(
+    //     "chatroom.receiver_id",
+    //     mainUserId
+    //   );
+    // })
+    // .orderBy("chatroom.updated_at", "desc");
 
     return chatHistory;
   };
