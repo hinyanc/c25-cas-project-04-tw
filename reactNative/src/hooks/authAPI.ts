f71bed0e70345c5a4d778203612c0a42424e6269
Chinny <learning20150133@gmail.com>
2023-06-15 16:14:24 +0800

    auth api

---
+++ reactNative/src/hooks/authAPI.ts
@@ -0,0 +1,23 @@
+import {REACT_APP_API_SERVER} from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function loaclLogin(email: string, password: string) {
+  const res = await fetch(`${REACT_APP_API_SERVER}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  const result = await res.json();
+  if(res.status ===200){
+    AsyncStorage.setItem('token',result.token)
+    return true
+  }else{
+    return false
+  }
+}
