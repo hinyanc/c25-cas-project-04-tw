d75bd345e59b01591851eef62b3c6ca4af36c982
dora dora <dora_1324@gmail.com>
2023-06-16 22:25:08 +0800

    tinder sql

--- express-server/services/discoverService.ts
+++ express-server/services/discoverService.ts
@@ -16,12 +16,10 @@ export class DiscoverService {
   };
 
   getAllProfile = async (userId: number) => {
-    const getAllProfileResult = await this.knex(userTable)
-      .select(
-        this.knex.raw(
-          /*sql*/ `users.id, is_pt, gender, username, profile_pic, (gym_center.gym_center) AS gym_center, (gym_location.gym_location) AS gym_location, json_agg(interest.name) AS interest_name, bio, `
-        )
-      )
+    const allProfiles = await this.knex(userTable)
+      .select(this.knex.raw(
+        /*sql*/ `users.id, is_pt, gender, username, profile_pic, (gym_center.gym_center) AS gym_center, (gym_location.gym_location) AS gym_location, json_agg(interest.name) AS interest_name, bio`))
+      .where("users.id", "!=", userId)
       .join("users_interest", "users_interest.users_id", "users.id")
       .join("interest", "users_interest.interest_id", "interest.id")
       .join("user_gym_center", "user_gym_center.users_id", "users.id")
@@ -32,55 +30,6 @@ export class DiscoverService {
         "user_gym_location.gym_location_id",
         "gym_location.id"
       )
-      .join("users_matching", function () {
-        this.on("users_matching.users_id", "=", "users.id").orOn(
-          "users_matching.matched_users_id",
-          "=",
-          "users.id"
-        );
-      })
-      .whereNot({ "users_matching.status": "matched" })
-      .andWhereNot({ "users.id": userId })
-      .groupBy(
-        "users.id",
-        "is_pt",
-        "gender",
-        "username",
-        "bio",
-        "profile_pic",
-        "gym_center.gym_center",
-        "gym_location.gym_location"
-        // "users_matching.status"
-      );
-    return getAllProfileResult;
-  };
-
-  getAllUsers = async (userId: number) => {
-    const getAllUsersProfileResult = await this.knex(userTable)
-      .select(
-        this.knex.raw(
-          /*sql*/ `users.id, is_pt, gender, username, profile_pic, (gym_center.gym_center) AS gym_center, (gym_location.gym_location) AS gym_location, json_agg(interest.name) AS interest_name, bio,(users_matching.status) AS match_status`
-        )
-      )
-      .join("users_interest", "users_interest.users_id", "users.id")
-      .join("interest", "users_interest.interest_id", "interest.id")
-      .join("user_gym_center", "user_gym_center.users_id", "users.id")
-      .join("gym_center", "user_gym_center.gym_center_id", "gym_center.id")
-      .join("user_gym_location", "user_gym_location.users_id", "users.id")
-      .join(
-        "gym_location",
-        "user_gym_location.gym_location_id",
-        "gym_location.id"
-      )
-      .join("users_matching", function () {
-        this.on("users_matching.users_id", "=", "users.id").orOn(
-          "users_matching.matched_users_id",
-          "=",
-          "users.id"
-        );
-      })
-      .whereNot({ "users_matching.status": "matched" })
-      .andWhereNot({ "users.id": userId })
       .groupBy(
         "users.id",
         "is_pt",
@@ -90,40 +39,28 @@ export class DiscoverService {
         "profile_pic",
         "gym_center.gym_center",
         "gym_location.gym_location",
-        "users_matching.status"
       )
-      .where("is_pt", "=", "false");
-    return getAllUsersProfileResult;
-  };
 
-  getAllPT = async (userId: number) => {
-    const firstStep = await this.knex(userTable)
-      .select("users.id")
-      .where("users.id", "!=", userId)
-      .andWhere("users.is_pt", "=", true);
-
-    console.log("step 1", firstStep);
-    let NotResult: any[] = [];
-    for (let item of firstStep) {
-      const secondStep = await this.knex("users_matching")
+    let filterMatchedAll: any[] = [];
+    for (let item of allProfiles) {
+      const matchedAll = await this.knex("users_matching")
         .select("matched_users_id")
         .where("users_id", "=", userId)
         .andWhere("matched_users_id", "=", item.id)
         .where("status", "=", "matched");
 
-      if (secondStep[0] != undefined) NotResult.push(secondStep[0]);
+      if (matchedAll[0] != undefined) filterMatchedAll.push(matchedAll[0]);
     }
+    console.log("firstRound", allProfiles);
+    console.log("not result ", filterMatchedAll);
 
-    console.log("not result ", NotResult);
 
-    console.log("firstRound", firstStep);
+    let allResult: any[] = [];
 
-    let filtered: any[] = [];
-
-    for (let candidate of firstStep) {
+    for (let candidate of allProfiles) {
       let flag = false;
 
-      for (let target of NotResult) {
+      for (let target of filterMatchedAll) {
         if (candidate.id == target.matched_users_id) {
           console.log("this is not", candidate.id);
           flag = true;
@@ -131,18 +68,139 @@ export class DiscoverService {
       }
       if (flag == true) {
       } else {
-        filtered.push(candidate);
+        allResult.push(candidate);
       }
     }
 
-    console.log("filtered", filtered);
-    // let filtered = firstStep.filter((item) => {
-    //   return filter_func(item);
-    // });
+    console.log("filtered", allResult);
 
-    // console.log("check filtered", filtered);
+    return allResult;
+  };
 
-    return firstStep;
+  getAllUsers = async (userId: number) => {
+    const allUsers = await this.knex(userTable)
+      .select(this.knex.raw(
+        /*sql*/ `users.id, is_pt, gender, username, profile_pic, (gym_center.gym_center) AS gym_center, (gym_location.gym_location) AS gym_location, json_agg(interest.name) AS interest_name, bio`))
+      .where("users.id", "!=", userId)
+      .andWhere("users.is_pt", "=", false)
+      .join("users_interest", "users_interest.users_id", "users.id")
+      .join("interest", "users_interest.interest_id", "interest.id")
+      .join("user_gym_center", "user_gym_center.users_id", "users.id")
+      .join("gym_center", "user_gym_center.gym_center_id", "gym_center.id")
+      .join("user_gym_location", "user_gym_location.users_id", "users.id")
+      .join(
+        "gym_location",
+        "user_gym_location.gym_location_id",
+        "gym_location.id"
+      )
+      .groupBy(
+        "users.id",
+        "is_pt",
+        "gender",
+        "username",
+        "bio",
+        "profile_pic",
+        "gym_center.gym_center",
+        "gym_location.gym_location",
+      )
+
+    let filterMatchedUsers: any[] = [];
+    for (let item of allUsers) {
+      const matchedUsers = await this.knex("users_matching")
+        .select("matched_users_id")
+        .where("users_id", "=", userId)
+        .andWhere("matched_users_id", "=", item.id)
+        .where("status", "=", "matched");
+
+      if (matchedUsers[0] != undefined) filterMatchedUsers.push(matchedUsers[0]);
+    }
+    console.log("firstRound", allUsers);
+    console.log("not result ", filterMatchedUsers);
+
+
+    let usersResult: any[] = [];
+
+    for (let candidate of allUsers) {
+      let flag = false;
+
+      for (let target of filterMatchedUsers) {
+        if (candidate.id == target.matched_users_id) {
+          console.log("this is not", candidate.id);
+          flag = true;
+        }
+      }
+      if (flag == true) {
+      } else {
+        usersResult.push(candidate);
+      }
+    }
+
+    console.log("filtered", usersResult);
+
+    return usersResult;
+  };
+
+  getAllPT = async (userId: number) => {
+    const allPT = await this.knex(userTable)
+      .select(this.knex.raw(
+        /*sql*/ `users.id, is_pt, gender, username, profile_pic, (gym_center.gym_center) AS gym_center, (gym_location.gym_location) AS gym_location, json_agg(interest.name) AS interest_name, bio`))
+      .where("users.id", "!=", userId)
+      .andWhere("users.is_pt", "=", true)
+      .join("users_interest", "users_interest.users_id", "users.id")
+      .join("interest", "users_interest.interest_id", "interest.id")
+      .join("user_gym_center", "user_gym_center.users_id", "users.id")
+      .join("gym_center", "user_gym_center.gym_center_id", "gym_center.id")
+      .join("user_gym_location", "user_gym_location.users_id", "users.id")
+      .join(
+        "gym_location",
+        "user_gym_location.gym_location_id",
+        "gym_location.id"
+      )
+      .groupBy(
+        "users.id",
+        "is_pt",
+        "gender",
+        "username",
+        "bio",
+        "profile_pic",
+        "gym_center.gym_center",
+        "gym_location.gym_location",
+      )
+
+    let filterMatchedPT: any[] = [];
+    for (let item of allPT) {
+      const matchedPT = await this.knex("users_matching")
+        .select("matched_users_id")
+        .where("users_id", "=", userId)
+        .andWhere("matched_users_id", "=", item.id)
+        .where("status", "=", "matched");
+
+      if (matchedPT[0] != undefined) filterMatchedPT.push(matchedPT[0]);
+    }
+    console.log("firstRound", allPT);
+    console.log("not result ", filterMatchedPT);
+
+
+    let ptResult: any[] = [];
+
+    for (let candidate of allPT) {
+      let flag = false;
+
+      for (let target of filterMatchedPT) {
+        if (candidate.id == target.matched_users_id) {
+          console.log("this is not", candidate.id);
+          flag = true;
+        }
+      }
+      if (flag == true) {
+      } else {
+        ptResult.push(candidate);
+      }
+    }
+
+    console.log("filtered", ptResult);
+
+    return ptResult;
   };
 
   updateLikeUser = async (userId: number, targetUserId: number) => {
